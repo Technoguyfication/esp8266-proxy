398090f0d1ea49f60581de2447384ec102d3621c
Hayden Andreyka <haydenandreyka@gmail.com>
2017-11-06 15:57:35 -0700

    some cleanup and magically everything is working

--- esp8266-proxy/esp8266_proxy.ino
+++ esp8266-proxy/esp8266_proxy.ino
@@ -1,27 +1,28 @@
 /*
-  ESP8266 Proxy
+ESP8266 Proxy
 
-  Made for WiFi MAC address spoofing
+Made for WiFi MAC address spoofing
 */
 
-#include "Settings.h"
+#include <WString.h>
+#include <ESP8266WiFi.h>
 
 // Feel free to change this to whatever is more convenient
 // Please note this may not match the bootloader baudrate of your chip
 // this could result in garbage being printed upon reset.
 #define SERIAL_BAUDRATE 115200
 
-Settings settings;
+const char* BroadcastSSID = "esp8266-proxy";
+const char* BroadcastPass = "thereisnospoon";
+const bool BroadcastHidden = false;
 
 void setup()
 {
 	Serial.begin(SERIAL_BAUDRATE);
 	Serial.println("\n\nStarting esp8266 proxy...");
 
-	settings.Load();
-
-	Serial.println("Loaded settings");
-	Serial.println("Using broadcast SSID \"" + settings.BroadcastSSID + "\" and password \"" + settings.BroadcastPassword + "\"");
+	Serial.println("Using broadcast SSID \"" + (String)BroadcastSSID + "\" and password \"" + BroadcastPass + "\"");
+	WiFi.softAP(BroadcastSSID, BroadcastPass, BroadcastHidden);
 }
 
 void loop()
